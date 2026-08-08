f2bfd9461cd36345ebf62bab934fe941af72da79
qboileau <quentin.boileau@gmail.com>
2026-08-09 00:54:55 +0200

    feat(schema): add vigie schema command

---
+++ cmd/vigie/schema.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fregateops/vigie/internal/dsl"
+	"github.com/spf13/cobra"
+)
+
+var schemaCmd = &cobra.Command{
+	Use:   "schema",
+	Short: "Print the test file JSON Schema",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Fprintf(os.Stdout, "%s\n", dsl.SchemaJSON())
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(schemaCmd)
+}
